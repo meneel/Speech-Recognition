f75545a8a3ea4a3c7b1f046fc6446e31de5c5857
Indraneel Dutta <meneel2000@gmail.com>
2021-09-23 14:44:46 +0530

    first commit

---
+++ Project/app.py
@@ -0,0 +1,39 @@
+import speech_recognition as sr
+from flask import logging, Flask, render_template, request, flash
+
+
+app = Flask(__name__)
+app.secret_key = "meneel"
+
+@app.route('/')
+def index():
+    flash(" Welcome to Indraneels's site")
+    return render_template('index.html')
+
+@app.route('/audio_to_text/')
+def audio_to_text():
+    flash(" Press Start to start recording audio and press Stop to end recording audio")
+    return render_template('audio_to_text.html')
+
+@app.route('/audio', methods=['POST'])
+def audio():
+    r = sr.Recognizer()
+    with open('upload/audio.wav', 'wb') as f:
+        f.write(request.data)
+  
+    with sr.WavFile('upload/audio.wav') as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data, language='en-IN', show_all=True)
+        print(text)
+        return_text = " Did you say : <br> "
+        try:
+            for num, texts in enumerate(text['alternative']):
+                return_text += str(num+1) +") " + texts['transcript']  + " <br> "
+        except:
+            return_text = " Sorry!!!! Voice not Detected "
+        
+    return str(return_text)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
